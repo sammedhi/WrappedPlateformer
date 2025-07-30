7ee829d5dfb265297661bf2c5f55df1ecc0ea05d
sammedhi <sammedhi2@hotmail.fr>
2025-07-30 13:08:16 +0200

    Write an editor window that provides way to control the WrapShader, for now it just give the ability to turn it no/off

---
+++ Assets/Script/Editor/WrappingControllerWindow.cs
@@ -0,0 +1,41 @@
+using UnityEditor;
+using UnityEngine;
+using WrapShader;
+
+namespace CircularPlatformer
+{
+    /// <summary>
+    /// Editor window that provides utilities to control the wrapping shaders
+    /// </summary>
+    class WrappingControllerWindow : EditorWindow
+    {
+        /// <summary>
+        /// Is world wrapping enabled
+        /// </summary>
+        [SerializeField] bool _wrappingEnabled;
+
+        [MenuItem("Window/Wrapping Controller Window")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(WrappingControllerWindow), true);
+        }
+
+        void OnGUI()
+        {
+            if (GUILayout.Toggle(_wrappingEnabled, "Enable Wrapping"))
+            {
+                if (!_wrappingEnabled) SceneView.RepaintAll();
+
+                _wrappingEnabled = true;
+                WrappingController.EnableWrapping();
+            }
+            else
+            {
+                if (!_wrappingEnabled) SceneView.RepaintAll();
+
+                _wrappingEnabled = false;
+                WrappingController.DisableWrapping();
+            }
+        }
+    }
+}
